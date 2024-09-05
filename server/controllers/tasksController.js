74648a22bbf287de2c3b4ecedb5bd0cbfa95fa9e
Zakari Adamu <ojonimizakari@gmail.com>
2024-09-05 21:50:21 +0100

    implement REST API for tasks with complete CRUD functionalities

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler");
 
+const Task = require("../models/taskModel");
+
 // @desc: Get tasks
 // @route: GET /api/tasks
 // @access: Private
 const getTasks = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Get tasks" });
+	const tasks = await Task.find();
+	res.status(200).json(tasks);
 });
 // @desc: Add tasks
 // @route: POST /api/tasks
@@ -14,19 +17,41 @@ const addTasks = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error("Please add task");
 	}
-	res.status(200).json({ message: "Add tasks" });
+
+	const task = await Task.create({
+		text: req.body.text,
+	});
+
+	res.status(200).json(task);
 });
 // @desc: Update tasks
 // @route: PUT /api/tasks
 // @access: Private
 const updateTask = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Update tasks" });
+	const task = await Task.findById(req.params.id);
+
+	if (!task) {
+		res.status(400);
+		throw new Error("Task not found");
+	}
+	const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	});
+
+	res.status(200).json(updatedTask);
 });
 // @desc: Delete task
 // @route: DELETE /api/tasks
 // @access: Private
 const deleteTask = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Delete task" });
+	const task = await Task.findByIdAndDelete(req.params.id);
+
+	if (!task) {
+		res.status(400);
+		throw new Error("Task not found");
+	}
+
+	res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
