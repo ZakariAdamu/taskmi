08fcce3a11315476ab3640dfb2139c033f10d9c1
Zakari Adamu <ojonimizakari@gmail.com>
2024-09-10 10:38:37 +0100

    update index.js adding an initial home route

--- server/index.js
+++ server/index.js
@@ -19,8 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/tasks", tasksRoute);
 app.use("/api/users", userRoute);
 
+// Home route for initial testing
+app.get("/", (req, res) => {
+	res.send("Hello from Node TaskMi API, AlhamdulilLah x3");
+});
+
 app.use(errorHandler);
 
 // Server/backend setup
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`server running on port ${port} shukran!`));
+app.listen(port, () => console.log(`server running at http://localhost:${port} shukran!`));
